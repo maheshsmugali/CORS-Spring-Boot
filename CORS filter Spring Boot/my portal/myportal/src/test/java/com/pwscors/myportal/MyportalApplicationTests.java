244726ee5b0ac621eadf9f3e8cbe25cb6bab21dd
maheshsmugali <122901007+maheshsmugali@users.noreply.github.com>
2023-02-18 23:39:15 +0530

    Add files via upload

---
+++ CORS filter Spring Boot/my portal/myportal/src/test/java/com/pwscors/myportal/MyportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwscors.myportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
